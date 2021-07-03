98990addab061d793d6b74aefefb756201f8cb6e
iCode_Rx <arababs2015@gmail.com>
2021-07-03 16:50:52 +0100

    solution

---
+++ index.js
@@ -0,0 +1,46 @@
+// para: s
+// return s         ss      sss         ssss       sssss
+// eg. a  aa        aaa       aaaa      aaaaa
+// pseudo code
+
+
+function fiveLine(s){
+          // trim d argument
+          s = s.trim();
+
+          // // loop 5 times
+          let k = '';
+
+          for(let i = 0; i < 5; i++) {
+          // for each subsequent loop add extra s and console log
+                    k = k + s
+
+                    console.log(k);
+
+          }
+}
+
+fiveLine("  a")
+fiveLine("\txy \n")
+fiveLine("           Ok               ")
+
+// using string template
+function fiveLines(s){
+          // trim d argument
+          s = `${s}`.trim()
+
+          // // loop 5 times
+          let k = '';
+
+          for(let i = 0; i < 5; i++) {
+          // for each subsequent loop add extra s and console log
+                    k = `${k + s}`
+
+                    console.log(k);
+
+          }
+}
+
+fiveLines("  a")
+fiveLines("\txy \n")
+fiveLines("           Ok               ")
